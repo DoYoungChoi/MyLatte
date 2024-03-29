57b2188aad7fb8917a3c9d2b3a9b54185962a196
최도영 <choi.doyoung@cashwalk.io>
2024-03-29 09:49:44 +0900

    Initial Commit

---
+++ MyLatte/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyLatte
+//
+//  Created by 최도영 on 3/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyLatte/MyLatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLatteApp.swift
+//  MyLatte
+//
+//  Created by 최도영 on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MyLatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
