a60122d3cca93466e51c357a697c70bdbb8b67cc
최도영 <choi.doyoung@cashwalk.io>
2024-03-29 11:58:13 +0900

    add: 배경 사진 변경 기능 추가

--- MyLatte/ContentView.swift
+++ MyLatte/ContentView.swift
@@ -6,18 +6,51 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @ObservedObject private var viewModel: ContentViewModel = .init()
+    private let width = UIScreen.main.bounds.size.width
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("latte\(viewModel.count)")
+                .resizable()
+                .scaledToFill()
+                .opacity(0.5)
+                .ignoresSafeArea()
+            
+            Image("latte\(viewModel.count)")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width)
         }
-        .padding()
     }
 }
+
+final class ContentViewModel: ObservableObject {
+    @Published var count: Int
+    var cancellables: Set<AnyCancellable>
+    
+    init() {
+        self.count = (1...12).randomElement() ?? 1
+        self.cancellables = []
+        changeImage()
+    }
+    
+    func changeImage() {
+        Timer.publish(every: 20, on: .main, in: .default)
+            .autoconnect()
+            .sink { [weak self] _ in
+                self?.count += 1
+                if self?.count ?? 13 > 12 {
+                    self?.count = 1
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
+
 
 #Preview {
     ContentView()
